4cf3416a3b5944bbe25d9ced3086410c6b9c95c3
Siddharth Manne <smanne@stanford.edu>
2026-02-14 15:16:41 -0800

    Delete time embedding variant

--- model_training_time_emb/train_model.py
+++
@@ -1,6 +0,0 @@
-from omegaconf import OmegaConf
-from rnn_trainer import BrainToTextDecoder_Trainer
-
-args = OmegaConf.load('rnn_args.yaml')
-trainer = BrainToTextDecoder_Trainer(args)
-metrics = trainer.train()
